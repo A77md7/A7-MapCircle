0cdb2c62b5cb01db745a38d8ea89df66438904fd
A7-Dev <129503548+A77md7@users.noreply.github.com>
2023-07-28 11:10:41 +0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,35 @@
+Config = {}
+
+
+Config.A7store = {
+    [1] = { -- ballas
+        ['Coords'] = vector4(4.39, -1830.93, 24.97, 120.84), -- الموقع
+        ['Colour'] = 50,-- اللون : https://docs.fivem.net/docs/game-references/blips/#blip-colors
+        ['size'] = 140,-- الشفافيه
+    },
+    [2] = { -- vagos
+        ['Coords'] = vector4(337.92, -2039.93, 21.24, 120.84), -- الموقع
+        ['Colour'] = 70,-- اللون : https://docs.fivem.net/docs/game-references/blips/#blip-colors
+        ['size'] = 140,-- الشفافيه
+    },
+    [3] = { -- families
+        ['Coords'] = vector4(-160.55, -1574.36, 35.08, 120.84), -- الموقع
+        ['Colour'] = 2,-- اللون : https://docs.fivem.net/docs/game-references/blips/#blip-colors
+        ['size'] = 140,-- الشفافيه
+    },
+    [4] = { -- bloods
+        ['Coords'] = vector4(-1569.21, -404.69, 62.55, 120.84), -- الموقع
+        ['Colour'] = 1,-- اللون : https://docs.fivem.net/docs/game-references/blips/#blip-colors
+        ['size'] = 140,-- الشفافيه
+    },
+    [5] = { -- crips
+        ['Coords'] = vector4(-361.32, 34.16, 48.0, 120.84), -- الموقع
+        ['Colour'] = 3,-- اللون : https://docs.fivem.net/docs/game-references/blips/#blip-colors
+        ['size'] = 140,-- الشفافيه
+    },
+    [6] = { -- mc
+        ['Coords'] = vector4(973.11, -118.77, 74.35, 120.85), -- الموقع
+        ['Colour'] = 1211,-- اللون : https://docs.fivem.net/docs/game-references/blips/#blip-colors
+        ['size'] = 140,-- الشفافيه
+    },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5' 
+
+author 'a77b'
+description 'A7 Store - https://discord.gg/sUU5ZYXT3b'
+version '1.0.0'
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+	'client/main.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
+
+lua54 'yes'
